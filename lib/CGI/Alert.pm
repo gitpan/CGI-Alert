0b500f84430f725516c55ce765898c387c3e0146
Eduardo Santiago Muñoz <esm@cpan.org>
2010-08-02 00:43:53 +0000

    Import of ESM/CGI-Alert-2.02 from CPAN.
    gitpan-cpan-distribution: CGI-Alert
    gitpan-cpan-version:      2.02
    gitpan-cpan-path:         ESM/CGI-Alert-2.02.tar.gz
    gitpan-cpan-author:       ESM
    gitpan-cpan-maturity:     released

--- lib/CGI/Alert.pm
+++ lib/CGI/Alert.pm
@@ -2,7 +2,7 @@
 #
 # CGI::Alert.pm  -  notify a human about errors/warnings in CGI scripts
 #
-# $Id: 79 $
+# $Id: 80 $
 #
 package CGI::Alert;
 
@@ -87,7 +87,7 @@ our @EXPORT_OK   = qw(http_die);
 our $ME = $ENV{REQUEST_URI} || $0 || "<???>";
 
 # Module version, on one line for MakeMaker
-our $VERSION = 2.01;
+our $VERSION = 2.02;
 
 ############
 #  import  #  If called with "use CGI::Alert 'foo@bar'", send mail to foo@bar
